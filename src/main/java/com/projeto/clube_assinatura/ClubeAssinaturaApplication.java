fa2df8d41be60b8c712898fddd55d0a3697535ae
FabioLuizz <fabioluis19341@gmail.com>
2025-05-29 18:44:38 -0300

    Inicializado projeto Clube de Assinatura com Spring Boot

---
+++ src/main/java/com/projeto/clube_assinatura/ClubeAssinaturaApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.clube_assinatura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClubeAssinaturaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClubeAssinaturaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projeto/clube_assinatura/ClubeAssinaturaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.clube_assinatura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClubeAssinaturaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
