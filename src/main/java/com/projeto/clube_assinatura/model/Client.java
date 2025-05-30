93b200d4774610a9dd8b6ed98acf4b1165803ae7
FabioLuizz <fabioluis19341@gmail.com>
2025-05-30 20:42:02 -0300

    Refactor

--- src/main/java/com/projeto/clube_assinatura/model/Client.java
+++ src/main/java/com/projeto/clube_assinatura/model/Client.java
@@ -3,6 +3,7 @@ package com.projeto.clube_assinatura.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,6 +16,7 @@ import lombok.Setter;
 @NoArgsConstructor
 public class Client {
 
+    @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long Id;
 
